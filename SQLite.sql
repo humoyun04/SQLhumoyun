c878f64deb1f72964e0d809677552f1557e95e63
humoyun04 <humoyunakbaraliyev2159555@gmail.com>
2022-11-08 10:09:21 +0500

    Men qoshdm

---
+++ SQLite.sql
@@ -0,0 +1,79 @@
+CREATE TABLE school(
+  student_id INT,
+  student_name VARCHAR(255),
+  grade INT,
+  univercity TEXT
+);
+
+
+INSERT INTO school VALUES(
+  '1',
+  'Humoyun Akbaraliyev',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '2',
+  'Mirafzal Baxodirov',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '3',
+  'Azamat Abduraimov',
+  '5',
+  'MGIMO'
+);
+
+INSERT INTO school VALUES(
+  '4',
+  'Orazali Abdugapparov',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '5',
+  'Diyorbek Abduqaimov',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '6',
+  'Asadbek Ergashev',
+  '5',
+  'MISIS'
+);
+
+INSERT INTO school VALUES(
+  '7',
+  'Axmatov Alimardon',
+  '5',
+  'Moliya'
+);
+
+INSERT INTO school VALUES(
+  '8',
+  'Abdirimov Siyovush',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '9',
+  'Fazliddin Mirzayev',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '10',
+  'Asilbek Abubakirov',
+  '4',
+  'Politex'
+);
+
+SELECT * FROM school;
